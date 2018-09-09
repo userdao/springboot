0850c7704fa23bfdc5880a9452accf6444627518
userdao <ciciselim@gmail.com>
2018-09-09 08:59:08 +0200

    added log4j instead system.out.println

--- src/main/java/de/bips/bootweb/BootwebApplication.java
+++ src/main/java/de/bips/bootweb/BootwebApplication.java
@@ -1,17 +1,20 @@
 package de.bips.bootweb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootwebApplication {
 
+  private static final Logger logger = LoggerFactory.getLogger(BootwebApplication.class);
 
 
   public static void main(String[] args) {
     SpringApplication.run(BootwebApplication.class, args);
 
-    System.out.println("Main Application is starting!");
+    logger.info("BootwebApplication is starting!");
 
   }
 }
